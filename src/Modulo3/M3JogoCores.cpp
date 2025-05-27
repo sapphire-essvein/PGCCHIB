4eb996654832236b1e5b931cd61780995232b413
sapphire-essvein <lucasessveins@gmail.com>
2025-05-26 21:17:58 -0300

    Ativ Jogo das cores

--- src/Modulo3/M3JogoCores.cpp
+++ src/Modulo3/M3JogoCores.cpp
@@ -1,52 +1,31 @@
-/* Hello Triangle - código adaptado de https://learnopengl.com/#!Getting-started/Hello-Triangle
- *
- * Adaptado por Rossana Baptista Queiroz
- * para a disciplina de Processamento Gráfico - Unisinos
- * Versão inicial: 7/4/2017
- * Última atualização em 13/08/2024
- *
- */
-
 #include <iostream>
 #include <string>
 #include <assert.h>
 #include <vector>
-
 using namespace std;
-
-// GLAD
 #include <glad/glad.h>
-
-// GLFW
 #include <GLFW/glfw3.h>
-
-// GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
 using namespace glm;
-
 #include <cmath>
 #include <ctime>
 
-// Protótipo da função de callback de teclado
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 void mouse_button_callback(GLFWwindow *window, int button, int action, int mods);
+bool verificarFimDoJogo();
 
-// Protótipos das funções
 GLuint createQuad();
 int setupShader();
 int setupGeometry();
 void eliminarSimilares(float tolerancia);
 
-// Dimensões da janela (pode ser alterado em tempo de execução)
 const GLuint WIDTH = 800, HEIGHT = 600;
 const GLuint ROWS = 6, COLS = 8;
 const GLuint QUAD_WIDTH = 100, QUAD_HEIGHT = 100;
 const float dMax = sqrt(3.0);
 
-// Código fonte do Vertex Shader (em GLSL): ainda hardcoded
 const GLchar *vertexShaderSource = R"(
 #version 400
 layout (location = 0) in vec3 position;
@@ -59,7 +38,6 @@ void main()
 }
 )";
 
-// Código fonte do Fragment Shader (em GLSL): ainda hardcoded
 const GLchar *fragmentShaderSource = R"(
 #version 400
 uniform vec4 inputColor;
@@ -84,62 +62,42 @@ vector<vec3> colors;
 int iColor = 0;
 int iSelected = -1;
 
-// Criação da grid de quadrados
+int tentativas = 0;
+int pontos = 0;
+bool jogoEncerrado = false;
+
 Quad grid[ROWS][COLS];
 
-// Função MAIN
 int main()
 {
-	//srand(glfwGetTime()); TODO - Ver como transformar em unsigned int
 	srand(time(0));
 
-	// Inicialização da GLFW
 	glfwInit();
 
-	// Muita atenção aqui: alguns ambientes não aceitam essas configurações
-	// Você deve adaptar para a versão do OpenGL suportada por sua placa
-	// Sugestão: comente essas linhas de código para desobrir a versão e
-	// depois atualize (por exemplo: 4.5 com 4 e 5)
-	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
-
-	// Essencial para computadores da Apple
-	// #ifdef __APPLE__
-	//	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	// #endif
-
-	// Criação da janela GLFW
 	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Jogo das cores! ❤️🩷🧡💛💚", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
-	// Fazendo o registro da função de callback para a janela GLFW
 	glfwSetKeyCallback(window, key_callback);
 	glfwSetMouseButtonCallback(window, mouse_button_callback);
 
-	// GLAD: carrega todos os ponteiros d funções da OpenGL
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 	}
 
-	// Obtendo as informações de versão
-	const GLubyte *renderer = glGetString(GL_RENDERER); /* get renderer string */
-	const GLubyte *version = glGetString(GL_VERSION);	/* version as a string */
+	const GLubyte *renderer = glGetString(GL_RENDERER);
+	const GLubyte *version = glGetString(GL_VERSION);
 	cout << "Renderer: " << renderer << endl;
 	cout << "OpenGL version supported " << version << endl;
 
-	// Definindo as dimensões da viewport com as mesmas dimensões da janela da aplicação
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
-	// Compilando e buildando o programa de shader
 	GLuint shaderID = setupShader();
 
 	GLuint VAO = createQuad();
 
-	// Inicializar a grid
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
@@ -158,39 +116,24 @@ int main()
 		}
 	}
 
-	// Triangle tri;
-	// tri.position = vec3(400.0,300.0,0.0);
-	// tri.dimensions = vec3(100.0,100.0,1.0);
-	// tri.color = vec3(colors[iColor].r, colors[iColor].g, colors[iColor].b);
-	// iColor = (iColor + 1) % colors.size();
-	// triangles.push_back(tri);
-
 	glUseProgram(shaderID);
 
-	// Enviando a cor desejada (vec4) para o fragment shader
-	// Utilizamos a variáveis do tipo uniform em GLSL para armazenar esse tipo de info
-	// que não está nos buffers
 	GLint colorLoc = glGetUniformLocation(shaderID, "inputColor");
 
-	// Matriz de projeção paralela ortográfica
-	// mat4 projection = ortho(-10.0, 10.0, -10.0, 10.0, -1.0, 1.0);
 	mat4 projection = ortho(0.0, 800.0, 600.0, 0.0, -1.0, 1.0);
 	glUniformMatrix4fv(glGetUniformLocation(shaderID, "projection"), 1, GL_FALSE, value_ptr(projection));
 
-	// Loop da aplicação - "game loop"
 	while (!glfwWindowShouldClose(window))
 	{
-		// Checa se houveram eventos de input (key pressed, mouse moved etc.) e chama as funções de callback correspondentes
 		glfwPollEvents();
 
-		// Limpa o buffer de cor
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // cor de fundo
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glLineWidth(10);
 		glPointSize(20);
 
-		glBindVertexArray(VAO); // Conectando ao buffer de geometria
+		glBindVertexArray(VAO);
 
 		if (iSelected > -1)
 		{
@@ -204,54 +147,61 @@ int main()
 			{
 				if (!grid[i][j].eliminated)
 				{
-					// Matriz de modelo: transformações na geometria (objeto)
-					mat4 model = mat4(1); // matriz identidade
-					// Translação
+					mat4 model = mat4(1);
 					model = translate(model, grid[i][j].position);
-					//  Escala
 					model = scale(model, grid[i][j].dimensions);
 					glUniformMatrix4fv(glGetUniformLocation(shaderID, "model"), 1, GL_FALSE, value_ptr(model));
 					glUniform4f(colorLoc, grid[i][j].color.r, grid[i][j].color.g, grid[i][j].color.b, 1.0f); // enviando cor para variável uniform inputColor
-					// Chamada de desenho - drawcall
-					// Poligono Preenchido - GL_TRIANGLES
+
 					glDrawArrays(GL_TRIANGLE_STRIP, 0, 6);
 				}
 			}
 		}
 
-		glBindVertexArray(0); // Desconectando o buffer de geometria
+		if (!jogoEncerrado && verificarFimDoJogo())
+		{
+			jogoEncerrado = true;
+			std::cout << "Pontos finais: " << pontos-(tentativas*10) << std::endl;
+		}
 
-		// Troca os buffers da tela
+		glBindVertexArray(0);
+
 		glfwSwapBuffers(window);
 	}
-	// Pede pra OpenGL desalocar os buffers
-	// glDeleteVertexArrays(1, &VAO);
-	// Finaliza a execução da GLFW, limpando os recursos alocados por ela
+
 	glfwTerminate();
 	return 0;
 }
 
-// Função de callback de teclado - só pode ter uma instância (deve ser estática se
-// estiver dentro de uma classe) - É chamada sempre que uma tecla for pressionada
-// ou solta via GLFW
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
-{
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (key == GLFW_KEY_R && action == GLFW_PRESS) {
+
+		tentativas = 0;
+		pontos = 0;
+		jogoEncerrado = false;
+		iSelected = -1;
+
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				grid[i][j].position = vec3(j * QUAD_WIDTH + QUAD_WIDTH / 2.0f, i * QUAD_HEIGHT + QUAD_HEIGHT / 2.0f, 0.0f);
+				grid[i][j].dimensions = vec3(QUAD_WIDTH, QUAD_HEIGHT, 1.0f);
+				grid[i][j].color = vec3(static_cast<float>(rand()) / RAND_MAX,
+										static_cast<float>(rand()) / RAND_MAX,
+										static_cast<float>(rand()) / RAND_MAX);
+				grid[i][j].eliminated = false;
+			}
+		}
+	}
 }
 
-// Esta função está basntante hardcoded - objetivo é compilar e "buildar" um programa de
-//  shader simples e único neste exemplo de código
-//  O código fonte do vertex e fragment shader está nos arrays vertexShaderSource e
-//  fragmentShader source no inicio deste arquivo
-//  A função retorna o identificador do programa de shader
 int setupShader()
 {
-	// Vertex shader
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
-	// Checando erros de compilação (exibição via log no terminal)
 	GLint success;
 	GLchar infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -261,11 +211,9 @@ int setupShader()
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
 				  << infoLog << std::endl;
 	}
-	// Fragment shader
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
-	// Checando erros de compilação (exibição via log no terminal)
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if (!success)
 	{
@@ -273,12 +221,10 @@ int setupShader()
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
 				  << infoLog << std::endl;
 	}
-	// Linkando os shaders e criando o identificador do programa de shader
 	GLuint shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
-	// Checando por erros de linkagem
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success)
 	{
@@ -302,8 +248,14 @@ void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
 		cout << xpos / QUAD_WIDTH << " " << ypos / QUAD_HEIGHT << endl;
 		int x = xpos / QUAD_WIDTH;
 		int y = ypos / QUAD_HEIGHT;
+		if (grid[y][x].eliminated == false)
+				{
+					pontos = pontos+10;
+				}
 		grid[y][x].eliminated = true;
-		iSelected = x + y * COLS; //indice linear do quadrado selecionado
+		iSelected = x + y * COLS;
+
+		tentativas++;
 	}
 }
 
@@ -312,42 +264,26 @@ GLuint createQuad()
 	GLuint VAO;
 
 	GLfloat vertices[] = {
-		// x    y    z
-		// T0
-		-0.5, 0.5, 0.0,	 // v0
-		-0.5, -0.5, 0.0, // v1
-		0.5, 0.5, 0.0,	 // v2
-		0.5, -0.5, 0.0	 // v3
+		-0.5, 0.5, 0.0,
+		-0.5, -0.5, 0.0,
+		0.5, 0.5, 0.0,
+		0.5, -0.5, 0.0
 	};
 
 	GLuint VBO;
-	// Geração do identificador do VBO
 	glGenBuffers(1, &VBO);
-	// Faz a conexão (vincula) do buffer como um buffer de array
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// Envia os dados do array de floats para o buffer da OpenGl
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Geração do identificador do VAO (Vertex Array Object)
 	glGenVertexArrays(1, &VAO);
-	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
-	// e os ponteiros para os atributos
+
 	glBindVertexArray(VAO);
-	// Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando:
-	//  Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
-	//  Numero de valores que o atributo tem (por ex, 3 coordenadas xyz)
-	//  Tipo do dado
-	//  Se está normalizado (entre zero e um)
-	//  Tamanho em bytes
-	//  Deslocamento a partir do byte zero
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
 	glEnableVertexAttribArray(0);
 
-	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice
-	// atualmente vinculado - para que depois possamos desvincular com segurança
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
 	glBindVertexArray(0);
 
 	return VAO;
@@ -368,10 +304,27 @@ void eliminarSimilares(float tolerancia)
 			float dd = d/dMax;
 			if (dd <= tolerancia)
 			{
+				if (grid[i][j].eliminated == false)
+				{
+					pontos = pontos+10;
+				}
 				grid[i][j].eliminated = true;
 			}
 
 		}
 	}
 	iSelected = -1;
+}
+
+bool verificarFimDoJogo()
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (!grid[i][j].eliminated)
+				return false;
+		}
+	}
+	return true;
 }
